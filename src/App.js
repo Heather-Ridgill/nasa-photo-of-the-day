2b130518a43b6169b0f7db8b00d294f36b516fbc
Heather-Ridgill <Heather.Ridgill@gmail.com>
2019-08-01 20:08:04 -0700

    API-CALL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
+
+console.log("");
+const [picture, setPictureState] = useState("");
 
 function App() {
+  useEffect(() => {
+    axios
+      .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+      .then(response => {
+        const data = response.data.message;
+        console.log("response");
+        //SAVE TO STATE:
+        // setNasa(data);
+      });
+  }, []);
+
   return (
     <div className="App">
       <p>
