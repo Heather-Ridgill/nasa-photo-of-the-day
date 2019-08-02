e7875b495e8ac8731fabb566a68134d365a02b6d
Heather-Ridgill <Heather.Ridgill@gmail.com>
2019-08-01 23:33:42 -0700

    Finished-MVP

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import Nasalist from "./components/NasaList";
+import NasaCard from "./components/NasaCard";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import "./index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Scientists!</h1>
+      <h2>We need to see those amazing NasaPics!</h2>
+      <Nasalist />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
